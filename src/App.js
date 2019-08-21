1f3204a749b302f551f1761fb5e5585e2ea580cc
zzzfisherzzz <zzzfisherzzz@gmail.com>
2019-08-21 13:26:06 +0300

    Start project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from "./components/Header/Header";
+import NavBar from "./components/NavBar/NavBar";
+import Profile from "./components/Profile/Profile";
+
+const App = () => {
+  return (
+    <div className="app-wrapper">
+        <Header/>
+        <NavBar/>
+        <Profile/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <header className="header">
+            <img src="https://calendarmedia.blob.core.windows.net/assets/3867334a-8bcc-4f44-9684-3fe63020ec24.png" alt=""/>
+        </header>
+    )
+
+}
+
+export default Header
